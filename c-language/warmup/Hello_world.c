38d0b37d1114f013e195cb1650e5689a507e322b
sakethreddypingili <pingilisakethreddy@gmail.com>
2025-11-20 00:28:00 +0530

    Write a C program to print the message: Hello World!
    Writing a C program to print "Hello World": no input is required, no additional constraints, and single-line printing exactly: "Hello World!".

---
+++ c-language/warmup/Hello_world.c
@@ -0,0 +1,20 @@
+
+// Write a C program to print the message: Hello World!
+
+// Input Format:-
+// No input is required.
+
+// Constraints:-
+// N/A
+
+// Output Format:-
+// Single line printing exactly: Hello World!
+
+#include <stdio.h>
+
+int main() {
+  
+    printf("Hello World!");
+  
+    return 0;
+}
