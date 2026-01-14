bd71072d9e5ffbaaeac9ab01e6d990056cb30ec3
Saketh Reddy Pingili <pingilisakethreddy@gmail.com>
2026-01-14 20:01:31 +0530

    Add program to swap and print two numbers
    Implement a program to read and print two numbers in swapped order.

---
+++ c/Swap_Two_Numbers/Swap_Two_Numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    // Print swapped values directly (no actual variable swapping needed)
+    printf("a = %d\nb = %d", b, a);
+    
+    return 0;
+}
